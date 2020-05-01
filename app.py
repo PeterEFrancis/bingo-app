1cdc9f98dc7d5060ab9f145eeae74dbde1b33b9e
Peter Francis <francisp336@gmail.com>
2020-05-01 17:27:13 -0400

    add card

--- app.py
+++ app.py
@@ -14,27 +14,39 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', alreadyChosen=[])
+    return render_template('index.html')
+
+
+
+@app.route('/caller/<string:alreadyChosen>')
+def caller(alreadyChosen):
+
+    alreadyChosenArray = []
+    square = ""
+
+    if alreadyChosen != "p":
+         alreadyChosenArray = [int(i) for i in re.split('_',alreadyChosen)[1:]]
+         pick = alreadyChosenArray[-1]
+         square = {1:'B',2:'I',3:'N',4:'G',5:'O'}[np.ceil(pick/15)] + f'{pick}'
+
+    return render_template('caller.html', square = square, alreadyChosenArray=alreadyChosenArray)
 
 
 
 
-@app.route('/play/<string:alreadyChosen>')
-def draw(alreadyChosen):
-    all = [i+1 for i in range(75)]
-    alreadyChosen = [int(i) for i in re.split('_',alreadyChosen)[1:]]
-    if len(alreadyChosen) == 75:
-        return redirect('/index')
+@app.route('/card')
+def card():
 
+    B = sorted(r.sample(range(1,16), 5))
+    I = sorted(r.sample(range(16,31), 5))
+    N = sorted(r.sample(range(31,46), 5))
+    G = sorted(r.sample(range(46,61), 5))
+    O = sorted(r.sample(range(61,76), 5))
 
-    open =  [i for i in all if i not in alreadyChosen]
+    cardArray = np.array([B, I, N, G, O]).T
+    cardArray[2][2] = 0
 
-    pick = open[r.randint(0,len(open)-1)]
-    alreadyChosen = alreadyChosen + [pick]
-
-    square = {1:'B',2:'I',3:'N',4:'G',5:'O'}[np.ceil(pick/15)] + f'{pick}'
-
-    return render_template('index.html', square=square, alreadyChosen=alreadyChosen)
+    return render_template('card.html', cardArray=cardArray.tolist())
 
 
 
